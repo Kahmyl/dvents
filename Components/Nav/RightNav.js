606f78cb3b141547dc7df2d94ca1fc9783d69682
Kahmyl <59609015+Kahmyl@users.noreply.github.com>
2022-01-15 21:25:24 +0100

    Update RightNav.js

--- Components/Nav/RightNav.js
+++ Components/Nav/RightNav.js
@@ -62,7 +62,7 @@ const RightNav = ({ open }) => {
       <Ul open={open}>
         <li><Link href="/"><a>Home</a></Link></li>
         <li><Link href="/Events"><a>Events</a></Link></li>
-        <li><Link href="/Bookings"><a>Tickets</a></Link></li>
+        <li><Link href="/Events/Bookings"><a>Tickets</a></Link></li>
         <li><Link href="/Events/createEvent"><a>New Event</a></Link></li>
         {user.username.username && <li onClick={Logout}><DropContainer>Logout<LogOutIcon/></DropContainer></li>}
         {!user.username.username && <li><Link href="/User/Login"><a>Sign In</a></Link></li>}
