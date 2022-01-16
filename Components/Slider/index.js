eb66d29ebeafff832d395f99bb2d462360f817d8
Kahmyl <59609015+Kahmyl@users.noreply.github.com>
2022-01-16 10:14:36 +0100

    Editing Carousel

--- Components/Slider/index.js
+++ Components/Slider/index.js
@@ -1,13 +1,27 @@
-import { useState } from "react";
-import { Image, NavButton, SlideWrapper } from "../Global";
+import { Image, SlideWrapper } from "../Global";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-const Slider = ({config}) => {
-    const [imageIndex, setImageIndex] = useState(0)
+const config = [
+  {
+    title: 'Sporting Events',
+    image: 'https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?cs=srgb&dl=pexels-sebastian-voortman-214574.jpg&fm=jpg'
+  },
+  {
+    title: 'Live Concerts',
+    image: 'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?cs=srgb&dl=pexels-josh-sorenson-976866.jpg&fm=jpg'
+  },
+  {
+    title: 'Art Conventions',
+    image: 'https://images.pexels.com/photos/794212/pexels-photo-794212.jpeg?cs=srgb&dl=pexels-ralph-chang-794212.jpg&fm=jpg'
+  },
+]
+
+const Slider = () => {
+
     return ( 
         <Carousel autoPlay={true} emulateTouch={true} infiniteLoop={true}>
-          {config.map(item =>(
+          {config && config.map(item =>(
             <SlideWrapper>
               <Image src={item.image}/>
               <h2 className="legend">{item.title}</h2>
--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,6 @@ import Navbar from '../Components/Nav/Navbar'
 import { Container, Wrapper } from '../Components/Global'
 import Slider from '../Components/Slider';
 
-const config = [
-  {
-    title: 'Sporting Events',
-    image: 'https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?cs=srgb&dl=pexels-sebastian-voortman-214574.jpg&fm=jpg'
-  },
-  {
-    title: 'Live Concerts',
-    image: 'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?cs=srgb&dl=pexels-josh-sorenson-976866.jpg&fm=jpg'
-  },
-  {
-    title: 'Art Conventions',
-    image: 'https://images.pexels.com/photos/794212/pexels-photo-794212.jpeg?cs=srgb&dl=pexels-ralph-chang-794212.jpg&fm=jpg'
-  },
-]
 
 export default function Home() {
   const [count, setCount] = useState(0)
@@ -25,7 +11,7 @@ export default function Home() {
        <Navbar/>
           <Container>
             <Wrapper>
-              <Slider config={config} />
+              <Slider />
             </Wrapper>
           </Container>
     </div>
