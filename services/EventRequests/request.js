3071b593f296e6b9417fb83c6b5777858a68115a
Kahmyl <59609015+Kahmyl@users.noreply.github.com>
2022-01-23 11:55:50 +0100

    Delete request.js

--- services/EventRequests/request.js
+++
@@ -1,8 +0,0 @@
-import {api} from '../api'
-import { GetEvents } from '../../EventQueriesqueries/EventQueries'
-
-export const fetchEvents = async () => {
-    const request = await api.post('/', { query: GetEvents })
-    const data = request.data.data
-    return data
-}
